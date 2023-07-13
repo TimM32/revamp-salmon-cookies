265066d9d44de7af2b889ba9d929fddd545e47b1
Tim Maupin <timmaupin@live.com>
2023-07-13 16:19:09 -0500

    refactoring and moving code into right folders

--- pages/sales-info.jsx
+++ pages/sales-info.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
-import { StoreForm } from '@/components/layout/'
 
 export default function SalesInfoPage() {
   return (
     <div>
       <h1>Sales Information</h1>
       <p>Display your sales information here</p>
-
-      <StoreForm />
     </div>
   );
 }
