b54a8049330f33d79d824c7bd458fd56bf5bbb73
Tim Maupin <timmaupin@live.com>
2023-07-13 16:28:16 -0500

    refactor and start converting code to next.js

--- pages/sales-info.jsx
+++ pages/sales-info.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import { StoreForm } from '@/components/store-info/store-form'
 
 export default function SalesInfoPage() {
   return (
     <div>
       <h1>Sales Information</h1>
       <p>Display your sales information here</p>
+
+      <StoreForm />
     </div>
   );
 }
